bbe422fb1f97add3f684d0e382e425cc80a246c2
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-05-17 01:21:40 +0530

    Navbar created

--- frontend/src/components/student/Navbar.jsx
+++ frontend/src/components/student/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import {assets} from "../../assets/assets"
 import { Link } from 'react-router-dom'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
   const isCourseListPage = location.pathname.includes('/course-list');
+
+  const {openSignIn} = useClerk();
+  const { user } = useUser();
 
   return (
     <div className={`flex justify-between items-center px-4 sm:px-10 md:px-14 lg:px-36 py-4 border-b ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
@@ -11,18 +15,29 @@ const Navbar = () => {
       {/* desktop view */}
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
         <div className='flex items-center gap-5'>
-          <button>Become Educator</button>
+          { user && <>
+            <button>Become Educator</button>
           | <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          }
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+        { user ? <UserButton /> :
+          <button onClick={() => openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>}
       </div>
       {/* mobile view */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
-        <div>
-          <button>Become Educator</button>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:text-sm'>
+          { user && <>
+            <button>Become Educator</button>
           | <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          }
         </div>
-        <button><img src={assets.user_icon}/></button>
+        {
+          user ? <UserButton /> :
+          <button onClick={() => openSignIn()}><img src={assets.user_icon}/></button>
+        }
+        
       </div>
     </div>
   )
