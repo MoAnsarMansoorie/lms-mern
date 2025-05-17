e581fcb7920c1e8b972e32b8e1a3fb264282baa3
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-05-17 23:39:45 +0530

    Created SearchBar

--- frontend/src/components/student/SearchBar.jsx
+++ frontend/src/components/student/SearchBar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
-const Searchbar = () => {
+const Searchbar = ({data}) => {
+  const navigate = useNavigate()
+  const [input, setInput] = useState(data ? data : '')
+
+  const onSearchHandler = (e) => {
+    e.preventDefault()
+    navigate("/course-list/" + input)
+  }
+
   return (
-    <div>Searchbar</div>
+    
+      <form onSubmit={onSearchHandler} className='max-w-xl w-full md:h-14 h-12 flex items-center justify-between bg-white rounded-full shadow-md border border-gray-500/20'>
+        <img src={assets.search_icon} alt='search-icon' className='md:w-auto w-10 px-3' />
+        <input 
+        onChange={(e )=> setInput(e.target.value)} value={input} type="text" placeholder='Search for courses, instructors, or topics' className='w-full h-full outline-none text-gray-500/80' />
+        <button type='submit' className='bg-blue-600 text-white px-7 md:px-10 py-2 mx-1 rounded-full'>Search</button>
+      </form>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "course-details-heading-small" : ["26px", "36px"],
+        "course-details-heading-large" : ["36px", "48px"],
+        "home-heading-small" : ["28px", "34px"],
+        "home-heading-large" : ["48px", "56px"],
+        "default" : ["16px", "24px"],
+        "default-small" : ["14px", "20px"],
+        "default-large" : ["20px", "28px"],
+      },
+    },
   },
   plugins: [],
 }
